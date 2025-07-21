8ae3b2b3acf0ae12f26490ff00d17b086570546c
Greatonic <awosusigabriel2019@gmail.com>
2025-07-21 10:59:22 +0100

    Fixes Commit

--- contracts/ReverseRegistrar.sol
+++ contracts/ReverseRegistrar.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.17;
 
 import "./GraphiteDNSRegistry.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
